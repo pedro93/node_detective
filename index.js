44bf652eff3d733910174b3539b9fc44729b985c
Pedro Silva <pedro.cls93@gmail.com>
2015-12-14 11:37:29 +0100

    [BUG FIX] Changed execution to analyse whether path is a file or a directory.

--- index.js
+++ index.js
@@ -13,28 +13,46 @@ var _ = require('lodash');
 var options = stdio.getopt({
     'directory': {key: 'd', default: '', description: 'Directory to run on, by default runs in current directory', args: 1},
     'list': {key: 'l', description: "Returns 1 module per line for automatic grep use"},
-    'local': {key: 'o', description: "Ignore ignore local requires, e.g: requires(./something)"}
+    'local': {key: 'o', description: "Ignore ignore local requires, e.g: requires(./something)"},
+    'version': {key: 'v', description: "Returns version number"}
 });
-   
-readDir(validatePath(), ['node_modules']).then(function(results){
-    if(options.list){
-       results.forEach(function(a){console.log(a);});
+
+run();
+
+function run(){
+    checkOptions();
+    var path = validatePath();
+    if(isDirectory(path)){
+        readDir(path, ['node_modules']).then(function(results){
+            if(options.list){
+               results.forEach(function(a){console.log(a);});
+           } else {
+               console.log(results);
+           }
+       });
     } else {
-       console.log(results);
+        readFile(path).then(function(result){
+            if(options.list){
+               results.forEach(function(a){console.log(a);});
+           } else {
+               console.log(result);
+           }
+       });
     }
-});
+}
+
 
 function readDir(path, ignoreList) {
     var deferred = Q.defer();
     recursive(path, ignoreList, function(err, files){
-       	var filePromises = files.map(function(file) {return readFile(file);});
-        Q.allSettled(filePromises).then(function (requires) {
-           
-            var results = _.uniq(_.flattenDeep(requires.filter(function(require){
-                 return require.state == 'fulfilled'  
-            }).map(function(a){
-                return a.value;   
-            })));
+        var filePromises = files.map(function(file) {return readFile(file);});
+        Q.allSettled(filePromises).then(function (result) {
+
+            var results = _.uniq(_.flattenDeep(result.filter(function(require){
+               return require.state == 'fulfilled'  
+           }).map(function(a){
+            return a.value;   
+        })));
             deferred.resolve(results);
         });
     });
@@ -43,21 +61,25 @@ function readDir(path, ignoreList) {
 
 function readFile(file) {
     var deferred = Q.defer();
-    fs.readFile(file, function (err, data) {
-        if (err) throw err;
-        var requires = detective(data);
-        if(requires.length == 0) {
-            deferred.reject();
-        } else {
-            if(options.local){
-                 requires = _.remove(requires, function(module){
+    if(isJavascriptFile(file)){
+        fs.readFile(file, function (err, data) {
+            if (err) throw err;
+            var result = detective(data);
+            if(result.length == 0) {
+                deferred.reject();
+            } else {
+                if(options.local){
+                   result = _.remove(result, function(module){
                     return module.charAt(0) !== '.';
                 });
-            }
-            deferred.resolve(requires);            
-        }
-    });
-	return deferred.promise;
+               }
+               deferred.resolve(result);            
+           }
+       });    
+    } else{
+        deferred.reject();
+    }
+    return deferred.promise;
 };
 
 function validatePath() {
@@ -71,4 +93,24 @@ function validatePath() {
     path.resolve( searchPath ) == path.normalize( searchPath );
     
     return searchPath;
+}
+
+function isDirectory(path_string) {
+    return fs.lstatSync(path_string).isDirectory();
+}
+
+function isJavascriptFile(file_path){
+    return path.extname(file_path) === '.js';
+}
+
+function checkOptions(){
+    if(options.version){
+        try {
+            var j = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json')) + '');
+            console.log(j.version);
+        } catch (ex) {
+            log.info('error reading version', ex);
+        }
+        process.exit();
+    }
 }
